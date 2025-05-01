bf23dfe8b93665ccc7aca5c2a9d4472762fa8660
rmc-8.com <kmprmc@gmail.com>
2025-05-01 19:03:18 +0900

    Adds PDF generation functionality with font support
    Implements a new client for creating PDFs from text input, allowing for customizable font usage.
    
    Handles cases where the specified font is missing and falls back to a default font.
    
    Creates an output directory for saving generated PDFs.
    
    Enhances main application to utilize the new PDF client.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pathlib
+
+import fire
+
+from bpdf import Client
+
+
+def get_font_path():
+    return pathlib.Path(__file__).parent / "font" / "Lora-Regular.ttf"
+
+
+def proc(text: str):
+    font_path = get_font_path()
+    c = Client(font_path)
+    c.run(text)
+
+
 def main():
-    pass
+    fire.Fire(proc)
 
 
 if __name__ == "__main__":
